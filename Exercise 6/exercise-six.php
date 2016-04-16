0b7b1c093c0ccba0a708eb0291942b9d1adc1a3c
jtf28 <jtf28@pitt.edu>
2016-04-16 12:09:21 -0400

    skeleton of exercise 6 added

---
+++ Exercise 6/exercise-six.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body style="color:white; background-color:pink;">
+        <div align="center">
+            <form method="post" action="./exercise-six.php">
+
+                <p>Enter A Computer Science Course To Search: </p>
+                <input type="text" placeholder="Enter Course Here" name="course">
+                <button type="submit" value="Submit" style="color:white; background-color:red;"> Submit </button>
+
+            </form>
+        </div>
+    </body>
+</html>
