fbdf72c5c9cdab7d4351011db966ed37ff4fa699
jtf28 <jtf28@pitt.edu>
2016-04-16 19:36:23 -0400

    finished Exercise 7

--- Exercise 7/toggle.php
+++ Exercise 7/toggle.php
@@ -1,19 +1,39 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     </head>
+
     <body style="color:lightgreen; background-color:purple;">
         <br><br>
         <div align="center">
-            <button onclick="toggleData()" style="color:purple; background-color:lightgreen;"> Toggle Data </button>
+            <button onclick="toggleData()" value="Toggle Data" type="button" style="color:purple; background-color:lightgreen;"> Toggle Data </button>
         </div>
-        
-        <div align="center" id="loadData"></div>
+        <br>
+
+        <div align="center" id="dataDisplay"></div>
 
         <script>
+            var dataDownloaded = false;
             function toggleData()
             {
+                if( !dataDownloaded )
+                {
+                    $.ajax( { url: './getData.php',
+                            type: "POST",
+                            data: { "file" : "file1.txt" },
+                            success: function( data )
+                                    {
+                                        dataDownloaded = true;
+                                        document.getElementById( "dataDisplay" ).innerHTML = data;
+                                    }
+                            });
+                }
 
+                else
+                {
+                    $( "#dataDisplay" ).toggle();
+                }
             }
         </script>
     </body>
