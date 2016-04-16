30e892cc1ee472193f0ea0c8f843a664ae3be73e
jtf28 <jtf28@pitt.edu>
2016-04-16 19:58:39 -0400

    skeleton of Exercise 8 added

---
+++ Exercise 8/quiz.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    </head>
+
+    <body style="color:red; background-color:lightblue;">
+        <div align="center">
+
+        </div>
+        <script>
+        </script>
+    </body>
+</html>
