552d16f1b69a0fa3baf496eb9c47e86f34beadec
jtf28 <jtf28@pitt.edu>
2016-04-16 12:46:04 -0400

    added a almost finished checkForm() function

--- Exercise 6/exercise-six.php
+++ Exercise 6/exercise-six.php
@@ -8,7 +8,7 @@
     </head>
     <body style="color:white; background-color:pink;">
         <div align="center">
-            <form method="post" action="./exercise-six.php">
+            <form method="post" onsubmit="checkForm()" action="./exercise-six.php">
 
                 <p>Enter A Computer Science Course To Search: </p>
                 <input type="text" placeholder="Enter Course Here" name="course">
@@ -16,5 +16,46 @@
 
             </form>
         </div>
+
+        <script>
+            function checkForm()
+            {
+                var course = document.getElementsByName( "course" )[ 0 ];
+                if( course.length !== 6 )
+                {
+                    return "Incorrect length entered!\n";
+                }
+
+                var department = course.substring( 0, 2 ).toUpperCase();
+                var level = parseInt( course.substring( 2, 3 ) );
+                var num = parseInt( course.substring( 3 ) );
+
+                var errors = "";
+                if( department !== "CS" )
+                {
+                    errors += "Incorrect department entered!\n";
+                }
+
+                if( isNaN( level ) || level < 0 || level > 3 )
+                {
+                    errors += "Incorrect degree level entered!\n";
+                }
+
+                if( isNaN( num ) || num < 0 || num > 999 )
+                {
+                    errors += "Incorrect course number entered!\n";
+                }
+
+                if( errors !== "" )
+                {
+                    return false;
+                }
+
+                else
+                {
+                    return true;
+                }
+            }
+        </script>
     </body>
 </html>
