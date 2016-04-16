75220e1aa94ee2ee17a1f1629d839dfed486466b
jtf28 <jtf28@pitt.edu>
2016-04-16 18:26:59 -0400

    skeleton of exercise 7 uploaded

---
+++ Exercise 7/toggle.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body style="color:lightgreen; background-color:purple;">
+        <br><br>
+        <div align="center">
+            <button onclick="toggleData()" style="color:purple; background-color:lightgreen;"> Toggle Data </button>
+        </div>
+        
+        <div align="center" id="loadData"></div>
+
+        <script>
+            function toggleData()
+            {
+
+            }
+        </script>
+    </body>
+</html>
