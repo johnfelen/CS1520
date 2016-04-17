b320017ca55aa6de87ef089da03bbd7f35b0d48c
jtf28 <jtf28@pitt.edu>
2016-04-17 01:59:24 -0400

    added the questions to DOM

--- Exercise 8/problem.js
+++ Exercise 8/problem.js
@@ -4,4 +4,22 @@ function Problem( question, answers, correctAnswer )
     this.answers = answers;
     this.correctAnswerIndex = correctAnswer;
 
+    this.getQuestion = getQuestion;
+    this.getAnswers = getAnswers;
+    this.getCorrectAnsIndex = getCorrectAnsIndex;
+
+    function getQuestion()
+    {
+        return question;
+    }
+
+    function getAnswers()
+    {
+        return answers;
+    }
+
+    function getCorrectAnsIndex()
+    {
+        return correctAnswerIndex;
+    }
 }
--- Exercise 8/quiz.php
+++ Exercise 8/quiz.php
@@ -5,12 +5,16 @@
         <script src="problem.js"></script>
     </head>
 
-    <body style="color:red; background-color:lightblue;">
+    <body style="color:gray; background-color:lightblue;">
         <br><br>
         <div align="center">
-            <button onclick="displayQuiz()" value="Display Quiz" type="button" style="color:orange;"> Display Quiz </button>
+            <button onclick="displayQuiz()" value="Display Quiz" type="button" style="color:gray;"> Display Quiz </button>
         </div>
         <br>
+
+        <div align="center" id="problems">
+
+        </div>
 
         <script>
             var problems = new Array();
@@ -22,28 +26,56 @@
 
                 xhttp.onload = function()
                 {
-                    var quiz = xhttp.responseXML;
-                    var problemsInXML = quiz.getElementsByTagName( "problem" );
+                    parseXML( xhttp );
 
-                    // I used http://www.w3schools.com/xml/tryit.asp?filename=try_dom_loadxmltext and http://www.w3schools.com/xml/books.xml to help me work out all the xml data parsing
-                    for( var i = 0; i < problemsInXML.length; i++ )
+                    var displayHTML = "";    //will be used all the HTML to concatenate all the problems and then change the innerHTML
+                    for( var i = 0; i < problems.length; i++ )
                     {
-                        var currQuestion =  problemsInXML[ i ].getElementsByTagName( "question" )[ 0 ].childNodes[ 0 ];
+                        var currProblem = "<div align=\"center\" id=\"problem" + i + "\">";
+                        currProblem += problems[ i ].getQuestion();
 
-                        var currAnswers = new Array();
-                        var currCorrectAns;
-                        for( var e = 0; e < currAnswers.length; e++ )
+                        //add all the answers to a select element that is inside currProblem div
+                        var answers = problems[ i ].getAnswers();
+                        currProblem += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<select>";
+                        for( var e = 0; e < answers.length; e++ )
                         {
-                            currAnswers[ e ] = problemsInXML[ i ].getElementsByTagName( "answer" )[ e ].childNodes[ 0 ];
-
-                            if( currAnswers[ e ] == problemsInXML[ i ].getElementsByTagName( "correct" )[ 0 ].childNodes[ 0 ] )
-                            {
-                                currCorrectAns = e;
-                            }
+                            currProblem += "<option value=\"" + answers[ e ] + "\">" + answers[ e ] + "</option>"
                         }
+                        currProblem += "</select>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
 
-                        problems[ i ] = new Problem( currQuestion, currAnswers, currCorrectAns );
+                        currProblem += "<button type=\"button\" id=\"question\"" + i + " style=\"color:gray;\"> Answer Question! </button></div><br>";
+                        displayHTML += currProblem;
                     }
+
+                    $( "#problems" ).html( displayHTML );
+                }
+            }
+
+            function parseXML( xhttp )
+            {
+                var quiz = xhttp.responseXML;
+                var problemsInXML = quiz.getElementsByTagName( "problem" );
+
+                // I used http://www.w3schools.com/xml/tryit.asp?filename=try_dom_loadxmltext and http://www.w3schools.com/xml/books.xml to help me work out all the xml data parsing
+                for( var i = 0; i < problemsInXML.length; i++ )
+                {
+                    var currQuestion =  problemsInXML[ i ].getElementsByTagName( "question" )[ 0 ].childNodes[ 0 ].nodeValue;
+
+                    var currAnswers = new Array();
+                    var numAns = problemsInXML[ i ].getElementsByTagName( "answer" ).length;
+                    var currCorrectAns;
+
+                    for( var e = 0; e < numAns; e++ )
+                    {
+                        currAnswers[ e ] = problemsInXML[ i ].getElementsByTagName( "answer" )[ e ].childNodes[ 0 ].nodeValue;
+
+                        if( currAnswers[ e ] == problemsInXML[ i ].getElementsByTagName( "correct" )[ 0 ].childNodes[ 0 ].nodeValue )
+                        {
+                            currCorrectAns = e;
+                        }
+                    }
+
+                    problems[ i ] = new Problem( currQuestion, currAnswers, currCorrectAns );
                 }
             }
         </script>
