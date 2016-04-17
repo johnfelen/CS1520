b59067421a8d303a46cbefb2856f58b071cd1250
jtf28 <jtf28@pitt.edu>
2016-04-17 12:39:12 -0400

    skeleton for Exercise 9 added

---
+++ Exercise 9/word-count.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script src="problem.js"></script>
+    </head>
+
+    <body style="color:gray; background-color:lightblue;">
+        <br><br>
+        <div align="center" id="problems">
+            <button onclick="displayWord()" value="Display Word" type="button" style="color:gray;"> Display Word </button>
+        </div>
+
+        <script>
+
+        </script>
+    </body>
+</html>
